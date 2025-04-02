bbcf7a48689427c534cafe9d7e6dab1a0f5caeb8
Sarthak Sambhav <samsarbro@gmail.com>
2025-04-02 22:24:45 +0530

    15-2

--- JDBC.java
+++ JDBC.java
@@ -1,26 +1,18 @@
-import java.sql.*;
-
-public class JDBC{
-  public static void main(String[] args){
-    try{
-      Class.forName("com.mysql.cj.jdbc.Driver");  // Register    // 1
-      System.out.println("11111)
-      Connection con = DriverManager.getConnection(
-        "jdbc:mysql: // localhost:3306 /friday" , "root","root");
-      //here sonoo is database name, root is username and password     // 2
-      System.out.println("22222");
-      Statement stmt = con.createStatement();   // 3
-      System.out.println("33333");  
-      ResultSet rs = stmt.executeQuery("select * from student");  // 4
-  
-      while(rs.next())
-      System.out.println(rs.getInt(1)+ " " + rs.getString + " " + rs.getString(3);
-      }
-      con.close();   //5
-  
-      }
-    catch(Exception e){
-      System.out.println("Shubham check your program, there is a small error");
-    }
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class CRUD_Test {
+  public static void main(String[] args) {
+    DB_Connection obj_DB_Connection = new DB_Connection();
+    System.out.println(obj_DB_Connection.connection());
   }
+  public Connection connection(){
+    Connection connection = null;
+    try{
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/saturday","root","root");
+    } catch(Exception e){
+      System.out.println(e);
+    }
+    return connection;
+}
 }
